03ed31200118de3224726cacd7eef5e95039f1f9
Mayur Pardeshi <mickeytechnical@gmail.com>
2021-07-14 10:27:44 +0530

    created db

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class Comment(db.Model):
     comment_author = relationship("User", back_populates="comments")
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     post = relationship("BlogPost", back_populates="comments")
-db.create_all()
+# db.create_all()
 
 
 def admin_only(f):
